a3ff1d4e08ec4c83ed8696a38da6f13fe89f8daa
GENIUS501 <39778070+GENIUS501@users.noreply.github.com>
2021-10-09 11:58:18 -0600

    ASDF

---
+++ Netpoints/Netpoints/Scripts/Roles/Edit.js
@@ -0,0 +1,55 @@
+﻿
+var Entity = $("model").data("entity");
+
+var rolesEdit = new Vue({
+    //Data
+    data: {
+        model: Entity,
+        formulario: "#FormRoles",
+    },
+    //Metodos
+    methods: {
+
+        Save: function () {
+
+            if (BValidateData(this.formulario)) {
+                Loading.fire("Guardando...");
+                axios.post("Roles/Save", this.model).then(function (get) {
+                    Loading.close();
+                    var result = get.data;
+
+                    if (result.CodeError == 0) {
+                        Toast.fire({
+                            icon: 'success',
+                            title: 'Registro Guardado'
+                        });
+                        setTimeout(function () {
+                            window.location.href = "../Roles"
+                        }, 1500)
+                    } else {
+                        Toast.fire({
+                            icon: 'error',
+                            title: result.MsgError
+                        });
+                    }
+                });
+
+            } else {
+
+                Toast.fire({
+                    icon: "error",
+                    title: "Porfavor Complete los campos requeridos!"
+                });
+            }
+        }
+
+    },
+    mounted: function () {
+        CreateValidator(this.formulario);
+    }
+    //create
+
+
+});
+
+rolesEdit.$mount("#RolesEdit");
