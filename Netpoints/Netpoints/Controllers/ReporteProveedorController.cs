532f42c73c13b08684e32121b021873a35cb08e4
GENIUS501 <39778070+GENIUS501@users.noreply.github.com>
2021-10-30 21:07:32 -0600

    Primer reporte.

---
+++ Netpoints/Netpoints/Controllers/ReporteProveedorController.cs
@@ -0,0 +1,22 @@
+﻿using Entidades;
+using Negocios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Netpoints.Controllers
+{
+    public class ReporteProveedorController : Controller
+    {
+        // GET: ReporteProveedor
+        public ActionResult Index()
+        {
+            NProveedores Negocios = new NProveedores();
+            List<EProveedores> Lista = new List<EProveedores>();
+            Lista = Negocios.Mostrar();
+            return View(Lista);
+        }
+    }
+}
