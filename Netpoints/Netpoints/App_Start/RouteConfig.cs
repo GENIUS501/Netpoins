82336e0439b0941d29e95e30062bc1ea0dc38ec9
GENIUS501 <39778070+GENIUS501@users.noreply.github.com>
2021-11-22 19:55:10 -0600

    a

--- Netpoints/Netpoints/App_Start/RouteConfig.cs
+++ Netpoints/Netpoints/App_Start/RouteConfig.cs
@@ -11,13 +11,13 @@ namespace Netpoints
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            //routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            //routes.MapRoute(
-            //    name: "Default",
-            //    url: "{controller}/{action}/{id}",
-            //    defaults: new { controller = "Acceso", action = "Login", id = UrlParameter.Optional }
-            //);
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Acceso", action = "Login", id = UrlParameter.Optional }
+            );
         }
     }
 }
